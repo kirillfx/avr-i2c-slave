8a6eda58d9cf5960e81b4f3d96ac80db2e3ef43b
Kyrylo Chaika <kirillfx@gmail.com>
2023-12-10 00:46:11 +0200

    feat: nix develop works

--- flake.nix
+++ flake.nix
@@ -3,10 +3,10 @@
     flake-utils.url = "github:numtide/flake-utils";
     naersk.url = "github:nix-community/naersk";
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    # ravedude.url = "github:Rahix/avr-hal?dir=ravedude";
+    ravedude-flake.url = "github:Rahix/avr-hal?dir=ravedude";
   };
 
-  outputs = { self, flake-utils, naersk, nixpkgs }:
+  outputs = { self, flake-utils, naersk, nixpkgs, ravedude-flake }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = (import nixpkgs) {
@@ -24,6 +24,7 @@
         # For `nix develop`:
         devShell = pkgs.mkShell {
           nativeBuildInputs = with pkgs; [ rustup rustc cargo rust-analyzer pkgsCross.avr.buildPackages.gcc avrdude udev ];
+          buildInputs = [ ravedude-flake.packages.${system}.default ];
           shellHook = ''
             export RAVEDUDE_PORT=/dev/tty.usbserial-0001
           '';
